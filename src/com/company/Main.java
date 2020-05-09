f7f99fb52570d448a8b0b6b8bd60a180367e0538
Nastya <realovka@mail.ru>
2020-05-10 01:02:51 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[][] array = new String[4][4];
+        int i, j;
+        for (i = 0; i < array.length; i++) {
+            for (j = 0; j < array.length; j++) {
+                array[i][j] = " ";
+            }
+        }
+
+        for (i = 0; i < array.length; i++) {
+            for (j = i; j < array.length; j++) {
+                array[i][j]="*";
+            }
+        }
+        for (i = 0; i < array.length; i++) {
+            for (j = 0; j < array.length; j++) {
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
